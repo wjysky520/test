5431c546aaeef4465ee982b7ab8b5ede0d68f016
王俊元 <wangjunyuan@talkweb.com.cn>
2023-02-27 15:16:04 +0800

    测试

--- test-git/src/main/java/com/wjysky/testgit/Test.java
+++ test-git/src/main/java/com/wjysky/testgit/Test.java
@@ -16,8 +16,8 @@ public class Test {
 
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
-        list.add(11);
-        list.add(22);
+        list.add(1111111);
+        list.add(2222222);
         list.add(33);
         list.add(44);
         list.add(55);
