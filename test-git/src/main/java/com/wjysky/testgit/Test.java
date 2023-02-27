7e5bb7d11a71a64035983f17a37796d6b8a09631
zhangqiao <zhangqiao@qq.com.cn>
2023-02-27 14:45:51 +0800

    测试

--- test-git/src/main/java/com/wjysky/testgit/Test.java
+++ test-git/src/main/java/com/wjysky/testgit/Test.java
@@ -21,6 +21,11 @@ public class Test {
         list.add(3);
         list.add(4);
         list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        list.add(9);
+        list.add(10);
         System.out.println(JSON.toJSONString(list));
     }
 }
