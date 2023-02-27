0065fc0fdc716ffc79ba47e9f6b7111fceb63153
zhangqiao <zhangqiao@qq.com.cn>
2023-02-27 14:54:52 +0800

    测试

--- test-git/src/main/java/com/wjysky/testgit/Test.java
+++ test-git/src/main/java/com/wjysky/testgit/Test.java
@@ -16,11 +16,11 @@ public class Test {
 
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
+        list.add(11);
+        list.add(22);
+        list.add(33);
+        list.add(44);
+        list.add(55);
         list.add(10);
         list.add(11);
         list.add(22);
