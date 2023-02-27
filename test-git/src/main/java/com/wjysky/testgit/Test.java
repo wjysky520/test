09f3bf41d64da2c4486e75d816260ab8453c8ca5
王俊元 <wangjunyuan@talkweb.com.cn>
2023-02-27 14:24:47 +0800

    测试

---
+++ test-git/src/main/java/com/wjysky/testgit/Test.java
@@ -0,0 +1,26 @@
+package com.wjysky.testgit;
+
+import com.alibaba.fastjson.JSON;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Test
+ *
+ * @author 王俊元（wangjunyuan@talkweb.com.cn）
+ * @date 2023-02-27 14:07:14
+ * @apiNote
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        System.out.println(JSON.toJSONString(list));
+    }
+}
